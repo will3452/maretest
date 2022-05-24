bfa8f827846e1939644cb88c8cd78423683cb1cb
leo27-corpuz <leocorpuz.jr27@gmail.com>
2022-05-24 16:31:05 +0800

    email verification

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes(['verify' => true]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+->name('home');
 
 Route::view('about', 'about')->name('about');
