64a8977761b63afffe0ef3a045a7bab630e3b9b6
leo27-corpuz <leocorpuz.jr27@gmail.com>
2022-06-02 14:01:10 +0800

    fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,10 +53,12 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('post') }}">{{ __('Post') }}</a>
+                                </li>
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
-
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::view('about', 'about')->name('about');
+
+Route::view('about', 'about')->name('about');
+
+Route::get('/post', [App\Http\Controllers\PostController::class, 'index'])->name('post');
