a5ddb2369f9e133bb931547d12fd3992848ccbb7
leo27-corpuz <leocorpuz.jr27@gmail.com>
2022-06-04 14:13:05 +0800

    announcement

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,8 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function announcement(){
+        return $this->hasMany(Announcement::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnnouncementController;
 
 Route::view('/', 'welcome');
 
@@ -10,3 +11,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
 ->name('home');
 
 Route::view('about', 'about')->name('about');
+Route::get('/announcement', [AnnouncementController::class, 'index'])->name('announcement');
+Route::post('/announcement', [AnnouncementController::class, 'store']);
